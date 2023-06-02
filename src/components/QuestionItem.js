ea801a83ac32a96afa8292125cc7ac7e86f1aef9
Stella Mazzillo <stellagress5@gmail.com>
2023-06-02 19:33:58 -0300

    complete react hooks fetch crud lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,19 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+
+
+
+  function handleDeleteClick(){
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method : "DELETE",
+    })
+    .then(res=>res.json())
+    .then(() => onDelete(question))
+  }
+
+
+
 
   return (
     <li>
@@ -17,9 +30,44 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
 
 export default QuestionItem;
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// function QuestionItem({ question }) {
+//   const { id, prompt, answers, correctIndex } = question;
+
+//   const options = answers.map((answer, index) => (
+//     <option key={index} value={index}>
+//       {answer}
+//     </option>
+//   ));
+
+//   return (
+//     <li>
+//       <h4>Question {id}</h4>
+//       <h5>Prompt: {prompt}</h5>
+//       <label>
+//         Correct Answer:
+//         <select defaultValue={correctIndex}>{options}</select>
+//       </label>
+//       <button>Delete Question</button>
+//     </li>
+//   );
+// }
+
+// export default QuestionItem;
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:4000/questions")
+    .then(res=> res.json())
+    .then(questions => setQuestions(questions))
+  
+  }, [])
+    
+  
+
+  function handleDeleteQuestion(deletedQuestion){
+    const updateQuestions = questions.filter(question => question.id !== deletedQuestion.id)
+    setQuestions(updateQuestions)
+  }
+
+
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question => (
+          <QuestionItem key={question.id} question={question} onDelete={handleDeleteQuestion}/>
+        ))}</ul>
     </section>
   );
 }
 
 export default QuestionList;
+
+
+
+
+// function handleDelete(questionId) {
+//   fetch(`http://localhost:4000/questions/${questionId}`, {
+//     method: "DELETE",
+//   })
+//     .then((res) => res.json())
+//     .then(() => {
+//       setQuestions(questions.filter((question) => question.id !== questionId));
+//     });
+// }
+
+
+
+
+// function handleDelete(){
+//   fetch(`http://localhost:4000/questions/${question.id}`, {
+//     method : "DELETE",
+//   })
+//   .then(res=>res.json())
+//   .then(()=>console.log(questions))
+// }
+
+
+
+
+// import React from "react";
+
+// function QuestionList() {
+//   return (
+//     <section>
+//       <h1>Quiz Questions</h1>
+//       <ul>{/* display QuestionItem components here after fetching */}</ul>
+//     </section>
+//   );
+// }
+
+// export default QuestionList;
